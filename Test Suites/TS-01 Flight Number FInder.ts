b879518a42a7382a9b1102ef1f111038cd7c8bb5
fahmiwazu <fahmiwazu@gmail.com>
2025-03-22 08:26:47 +0700

    FEAT: add Test Suite Flight Number Finder

---
+++ Test Suites/TS-01 Flight Number FInder.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS-01 Flight Number FInder</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>0c922866-6618-424a-81a5-b80941f9c26a</testSuiteGuid>
+   <testCaseLink>
+      <guid>e8bd62f0-6815-46e2-a411-366ff167af8d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC-01 Flight Finder</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
